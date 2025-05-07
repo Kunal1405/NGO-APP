1a3231d3c5fb37b6f60794e9c9cf7cb57613b4c8
kunal1405 <kunalsangwan0980@gmail.com>
2025-05-07 08:36:38 +0530

    adding validation

--- src/main/java/com/Kunal/Login/Controller/AuthController.java
+++ src/main/java/com/Kunal/Login/Controller/AuthController.java
@@ -6,19 +6,22 @@ import com.Kunal.Login.Service.UserService;
 import com.Kunal.Login.dto.AuthResponse;
 import com.Kunal.Login.dto.LoginRequest;
 import com.Kunal.Login.dto.SignupRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@Validated
 public class AuthController {
 
     private final UserService userService;
     private final JwtService jwtService;
 
     @PostMapping("/signup")
-    public String signup(@RequestBody SignupRequest request) {
+    public String signup(@RequestBody @Valid SignupRequest request) {
         userService.signup(request);
         return "Signup successful";
     }
--- src/main/java/com/Kunal/Login/dto/SignupRequest.java
+++ src/main/java/com/Kunal/Login/dto/SignupRequest.java
@@ -1,12 +1,18 @@
 package com.Kunal.Login.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class SignupRequest {
+    @NotNull(message = "First name cannot be null")
     private String firstName;
+
     private String lastName;
+
+    @Email(message = "Invalid email format")
     private String email;
     private String password;
 }
